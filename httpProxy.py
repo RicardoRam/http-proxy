5c9d71f2045df6716dcfea9be6dc98e6dc38c6d3
Ricardo Ramnarine <ricardo.ramnarine@mail.utoronto.ca>
2019-11-21 10:42:14 -0500

    added documentation for endpoint

--- httpProxy.py
+++ httpProxy.py
@@ -5,6 +5,20 @@ app = flask.Flask(__name__)
 
 @app.route('/proxy/<path:path>', methods=['GET', 'POST'])
 def _proxy(path):
+  """Call this proxy API endpoint '/proxy/<path>' by passing in a path that is a
+  URL you want to make a request to. Handles either a GET or POST request,
+  otherwise, returns an empty string.
+
+  GET: Updates the User-Agent header to the caller's User-Agent and returns the
+    updated response received from the <url>'s server.
+  POST: Updates the User-Agent header to the caller's User-Agent, adds form
+    data given, and returns the updated response received from the <url>'s server.
+
+  :param path: the URL you want to make a request to
+  :type path: string
+  :return: a modified response depending on the type of request
+  :rtype: string
+  """
   agent = {'User-Agent': flask.request.headers.get('User-Agent')}
 
   if flask.request.method == 'GET':
